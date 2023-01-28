a181c077922e57d7a4c631fd1d488e9bf8f2046f
Malte Franken <exxamalte@users.noreply.github.com>
2023-01-28 18:21:32 +1100

    Improve test coverage (#6)

---
+++ tests/test_init.py
@@ -0,0 +1,7 @@
+"""Tests for the INGV Centro Nazionale Terremoti (Earthquakes) QuakeML feed general setup."""
+from aio_quakeml_ingv_centro_nazionale_terremoti_client import __version__
+
+
+def test_version():
+    """Test for version tag."""
+    assert __version__ is not None

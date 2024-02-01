86bc97ec394f2080aeb097b56c0e735241397dac
Malte Franken <exxamalte@users.noreply.github.com>
2024-02-02 00:54:52 +1100

    code quality improvements

--- aio_quakeml_ingv_centro_nazionale_terremoti_client/__init__.py
+++ aio_quakeml_ingv_centro_nazionale_terremoti_client/__init__.py
@@ -1,3 +1,3 @@
 """aio_quakeml_ingv_centro_nazionale_terremoti_client library."""
-from .feed import IngvCentroNazionaleTerremotiQuakeMLFeed  # noqa
-from .feed_manager import IngvCentroNazionaleTerremotiQuakeMLFeedManager  # noqa
+from .feed import IngvCentroNazionaleTerremotiQuakeMLFeed  # noqa: F401
+from .feed_manager import IngvCentroNazionaleTerremotiQuakeMLFeedManager  # noqa: F401

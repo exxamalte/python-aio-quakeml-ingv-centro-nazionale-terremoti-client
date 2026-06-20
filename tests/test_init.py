0531bdb557192399d048b9f1abe1797aeb6731e9
Malte Franken <exxamalte@users.noreply.github.com>
2026-06-20 22:26:34 +1000

    Migrate to pyproject.toml (#32)
    * expand pyproject.toml
    
    * remove unnecessary files
    
    * modify pipeline
    
    * exclude tests from packaging

--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-"""Tests for the INGV Centro Nazionale Terremoti (Earthquakes) QuakeML feed general setup."""
-
-from aio_quakeml_ingv_centro_nazionale_terremoti_client import __version__
-
-
-def test_version():
-    """Test for version tag."""
-    assert __version__ is not None
